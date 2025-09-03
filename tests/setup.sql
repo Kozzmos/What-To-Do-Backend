9c3f651b4e4162a89da30e759bee7ccfe56ab98b
Akif Eren Ertuğrul <akiferenertugrul@gmail.com>
2025-09-03 23:07:04 +0300

    setup.sql güncellendi

--- tests/setup.sql
+++ tests/setup.sql
@@ -1,9 +1,18 @@
-CREATE TABLE IF NOT EXISTS todos (
+-- list tablosu
+CREATE TABLE IF NOT EXISTS list (
     id SERIAL PRIMARY KEY,
-    list_id INT NOT NULL,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    active BOOLEAN DEFAULT TRUE
+);
+
+-- todo tablosu
+CREATE TABLE IF NOT EXISTS todo (
+    id SERIAL PRIMARY KEY,
+    list_id INT NOT NULL REFERENCES list(id) ON DELETE CASCADE,
     text VARCHAR(255) NOT NULL,
     status VARCHAR(50) DEFAULT 'New',
-    active BOOLEAN DEFAULT true,
+    active BOOLEAN DEFAULT TRUE,
     due_date TIMESTAMP NULL,
     tags JSONB DEFAULT '[]'::jsonb
 );
